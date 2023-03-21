7191240771b024d620db3166ba7f4bbec3cd8416
Hugo Salinas <anguyapy@gmail.com>
2023-03-21 09:13:04 -0300

    Add buttons and text box

--- src/main/java/com/example/application/views/main/MainView.java
+++ src/main/java/com/example/application/views/main/MainView.java
@@ -35,34 +35,66 @@ public class MainView extends HorizontalLayout {
 package com.example.application.views.main;
 
 import com.vaadin.flow.component.Key;
+import com.vaadin.flow.component.Text;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.checkbox.Checkbox;
+import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.component.textfield.TextFieldVariant;
 import com.vaadin.flow.router.Route;
 
 @Route("") 
 public class MainView extends VerticalLayout { 
-
+     
   public MainView() {
-    VerticalLayout todosList = new VerticalLayout(); 
-    TextField taskField = new TextField(); 
-    Button addButton = new Button("Add"); 
-    addButton.addClickListener(click -> { 
+
+     TextField username = new TextField();
+     username.setPrefixComponent(VaadinIcon.USER.create());
+     username.setLabel("Username");
+     username.setValue("maverick");
+     add(username);
+
+     TextField email = new TextField();
+     email.setSuffixComponent(new Div(new Text("@example.com")));
+     email.setLabel("Email Address");
+     email.setValue("michael");
+     email.addThemeVariants(TextFieldVariant.LUMO_ALIGN_RIGHT);
+     email.setMaxLength(7);
+     add(email);
+
+     /*
+     TextField right = new TextField();
+     right.setValue("value");
+     right.addThemeVariants(TextFieldVariant.LUMO_ALIGN_RIGHT);
+
+     TextField textField = new TextField();
+     textField.setLabel("Street Address");
+     textField.setValue("Ruukinkatu 2");
+     textField.setClearButtonVisible(true);
+     textField.setPrefixComponent(VaadinIcon.MAP_MARKER.create());
+     add(textField);
+     */
+     
+     VerticalLayout todosList = new VerticalLayout(); 
+     TextField taskField = new TextField(); 
+     Button addButton = new Button("Agregar"); 
+     addButton.addClickListener(click -> { 
       Checkbox checkbox = new Checkbox(taskField.getValue());
       todosList.add(checkbox);
-    });
-    addButton.addClickShortcut(Key.ENTER); 
+     });
+     addButton.addClickShortcut(Key.ENTER); 
 
-    add( 
-      new H1("Vaadin Todo"),
+     add( 
+      new H1("Vaadin TODO"),
       todosList,
       new HorizontalLayout(
         taskField,
         addButton
       )
-    );
+     );
   }
 }
